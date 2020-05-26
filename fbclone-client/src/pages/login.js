c0b1b70bff383de77a0a4997881431db3edb5d87
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-05-26 22:53:52 +0800

    login page with helper text and get api from firebase

--- fbclone-client/src/pages/login.js
+++ fbclone-client/src/pages/login.js
@@ -1,13 +1,121 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { TextField, Grid, Typography, Button } from "@material-ui/core";
+import PropTypes from "prop-types";
+import AppIcon from "../images/logo.png";
+import axios from "axios";
+const styles = {
+  form: {
+    TextAlign: "center",
+  },
+  image: {
+    margin: "20px auto 20px auto",
+  },
+  pageTitle: {
+    margin: "0 auto 20px auto",
+  },
+  textField: {
+    margin: "20px auto 20px auto",
+  },
+  button: {
+    marginTop: 20,
+  },
+};
 
 class login extends Component {
-    render() {
-        return (
-            <div>
-                <h1>login</h1>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+          console.log(err)
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="Fire" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+             <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login
+            </Button>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
-export default login
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(login);
