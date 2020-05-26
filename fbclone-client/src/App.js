244805a2841fb0050f0853b2f3ba35198c13fb93
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-05-26 18:51:27 +0800

    fetch firebase api and show scream on react frontend

--- fbclone-client/src/App.js
+++ fbclone-client/src/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MuiThemProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //Components
-import NavBar from './components/Navbar'
+import NavBar from "./components/Navbar";
 
 //Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#8ae4ff',
+      main: '#50b2f4',
+      dark: '#0083c1',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#e1684f',
+      main: '#aa3925',
+      dark: '#740000',
+      contrastText: '#ffffff',
+    },
+  },
+    typography:{
+      userNextVariants: true
+    }
+});
 
 class App extends Component {
   render() {
     return (
+      <MuiThemProvider theme ={theme}>
       <div>
         <Router>
           <NavBar />
@@ -26,8 +47,9 @@ class App extends Component {
           </div>
         </Router>
       </div>
+      </MuiThemProvider>
     );
   }
 }
 
-export default App
+export default App;
