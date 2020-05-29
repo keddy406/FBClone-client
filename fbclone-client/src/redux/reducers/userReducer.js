00431db0370e11ebfa65613ed4a6c5dcece67ea5
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-05-29 19:50:28 +0800

    data reducer

--- fbclone-client/src/redux/reducers/userReducer.js
+++ fbclone-client/src/redux/reducers/userReducer.js
@@ -3,12 +3,14 @@ import {
   SET_USER,
   SET_AUTHENTICATED,
   SET_UNAUTHENTICATED,
-  LOADING_USER
+  LOADING_USER,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM
 } from "../types";
 
 const initialState = {
   authenticated: false,
-  loading:false,
+  loading: false,
   credentials: {},
   likes: [],
   notifications: [],
@@ -26,14 +28,32 @@ export default function (state = initialState, action) {
     case SET_USER:
       return {
         authenticated: true,
-        loading:false,
+        loading: false,
         ...action.payload,
       };
-      case LOADING_USER:
-        return{
+    case LOADING_USER:
+      return {
         ...state,
-        loading:true
-      }
+        loading: true,
+      };
+    case LIKE_SCREAM:
+      return {
+        ...state,
+        likes: [
+          ...state.likes,
+          {
+            userHandle: state.credentials.handle,
+            screamId: action.payload.screamId,
+          },
+        ],
+      };
+    case UNLIKE_SCREAM:
+      return {
+        ...state,
+        likes: state.likes.filter(
+          (likes) => likes.screamId === action.payload.screamId
+        ),
+      };
     default:
       return state;
   }
